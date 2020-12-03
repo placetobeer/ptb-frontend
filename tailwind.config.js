2a6314607a86bb52bc81a5aec6d64aba5f65b551
lucie <weber_lucie@web.de>
2020-12-03 23:58:35 +0100

    installed tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ptb-orange': '#dd6e42',
+        'ptb-sand': '#e8dab2',
+        'ptb-dark-blue': '#4f6d7a',
+        'ptb-blue': '#c0d6df',
+        'ptb-grey': '#eaeaea'
+      }
+    },
   },
   variants: {
     extend: {},
