de8224e3a4cca1fb577072f60860e9635d707e8a
lucie <weber_lucie@web.de>
2021-02-14 20:40:00 +0100

    PTB-91: add component test

--- src/app/hubpage/group-navigation-bar/create-group-popup/create-group-popup.component.spec.ts
+++ src/app/hubpage/group-navigation-bar/create-group-popup/create-group-popup.component.spec.ts
@@ -1,14 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import { CreateGroupPopupComponent } from './create-group-popup.component';
+import {PopupService} from '../../../popups/popup.service';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {DataService} from '../../../services/data.service';
 
 describe('CreateGroupPopupComponent', () => {
   let component: CreateGroupPopupComponent;
   let fixture: ComponentFixture<CreateGroupPopupComponent>;
+  let element;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateGroupPopupComponent ]
+      declarations: [ CreateGroupPopupComponent ],
+      providers: [
+        {provide: HttpClient},
+        {provide: HttpHandler},
+        {provide: PopupService,
+          useClass: class {
+            close = jasmine.createSpy('close');
+          }
+        },
+        {provide: DataService,
+          useClass: class  {
+            createGroup = jasmine.createSpy('createGroup');
+          }
+        }
+      ]
     })
     .compileComponents();
   });
@@ -16,10 +34,35 @@ describe('CreateGroupPopupComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateGroupPopupComponent);
     component = fixture.componentInstance;
+    element = fixture.nativeElement;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should close popup when cancelButton is clicked', () => {
+    const popupservice = fixture.debugElement.injector.get(PopupService);
+    component.onButtonClick('cancel');
+    expect(popupservice.close).toHaveBeenCalledWith('create-group');
+  });
+
+  it('should close popup when createButton is clicked', () => {
+    const popupservice = fixture.debugElement.injector.get(PopupService);
+    component.onButtonClick('create');
+    expect(popupservice.close).toHaveBeenCalledWith('create-group');
+  });
+
+  it('should create group when createButton is clicked', () => {
+    const dataservice = fixture.debugElement.injector.get(DataService);
+    component.onButtonClick('create');
+    expect(dataservice.createGroup).toHaveBeenCalledWith(dataservice.userId, '');
+  });
+
+  it('should clear input when createButton is clicked', () => {
+    component.onButtonClick('create');
+    expect(element.querySelector('#groupNameField').innerText).toBe('');
+  });
+
 });
