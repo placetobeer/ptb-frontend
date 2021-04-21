25c18ec6281b5bb91888bcaae0e967935a8a7abd
lucie <weber_lucie@web.de>
2021-04-21 23:12:25 +0200

    PTB-94: add invitation service

---
+++ src/app/services/invitation.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {Subject} from 'rxjs';
+import {Invitation} from '../entities/invitation.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InvitationService {
+  invitationEmitter = new Subject<Invitation>();
+  invitationList: Invitation[] = [];
+  invitationChanged = new Subject<Invitation[]>();
+  constructor() { }
+
+  addInvitation(newInvitation: Invitation): void {
+    this.invitationList.push(newInvitation);
+    this.invitationChanged.next(this.invitationList.slice());
+    this.invitationEmitter.next(newInvitation);
+  }
+
+  getInvitations(): Invitation[]{
+    return this.invitationList.slice();
+  }
+}
