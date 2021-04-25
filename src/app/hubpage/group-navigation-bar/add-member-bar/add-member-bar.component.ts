b2301bd31900c171730209b5f175aa2e3f88fcdc
lucie <weber_lucie@web.de>
2021-04-25 12:39:05 +0200

    PTB-94: add email already in list validation

--- src/app/hubpage/group-navigation-bar/add-member-bar/add-member-bar.component.ts
+++ src/app/hubpage/group-navigation-bar/add-member-bar/add-member-bar.component.ts
@@ -3,6 +3,7 @@ import {NgForm} from '@angular/forms';
 import {Invitation} from '../../../requests/invitation-request.model';
 import {User} from '../../../entities/user.model';
 import {InvitationService} from '../../../services/invitation.service';
+import {PopupHelperService} from '../../../popups/popup-helper.service';
 
 @Component({
   selector: 'app-add-member-bar',
@@ -14,13 +15,30 @@ export class AddMemberBarComponent implements OnInit {
 
   // TODO: replace Mock
   owner = new User(22, 'Hugo Boss');
-  constructor(private invitationService: InvitationService) { }
+  constructor(private invitationService: InvitationService, private popupHelperService: PopupHelperService) { }
 
   ngOnInit(): void {
   }
 
   onAddMember(): void {
     const newInvitation = new Invitation(0, this.owner, this.subForm.value.email, this.subForm.value.grantAdminRole);
-    this.invitationService.addInvitation(newInvitation);
+    if (this.validateAlreadyAdded()) {
+      this.invitationService.addInvitation(newInvitation);
+    } else {
+      this.openErrorPopup();
+    }
+  }
+
+  private validateAlreadyAdded(): boolean {
+    for (const invitation of this.invitationService.invitations) {
+      if (this.subForm.value.email === invitation.email) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private openErrorPopup(): void {
+    this.popupHelperService.openError('You have already added this User.');
   }
 }
