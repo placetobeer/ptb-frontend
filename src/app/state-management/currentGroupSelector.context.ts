0e97777051e27f44ae831bd424216c48be3a5d28
lucie <weber_lucie@web.de>
2021-05-19 21:09:02 +0200

    PTB-172: fix Injection error

--- src/app/state-management/currentGroupSelector.context.ts
+++ src/app/state-management/currentGroupSelector.context.ts
@@ -1,5 +1,9 @@
 import {CurrentGroupState} from "./currentGroupState.interface";
+import {Injectable} from "@angular/core";
 
+@Injectable({
+  providedIn: 'root'
+})
 export class CurrentGroupSelector {
   private currentGroupState: CurrentGroupState;
 
