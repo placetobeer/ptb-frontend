2b4064934148eeebcd9f4d8d6890b44a01328f19
bloup-dv <naujox.tom@gmail.com>
2021-05-28 01:48:38 +0200

    PTB-153: extended user.model.ts by adding email, token and tokenExpirationDate

--- src/app/entities/user.model.ts
+++ src/app/entities/user.model.ts
@@ -1,9 +1,22 @@
 export class User {
   id: number;
   name: string;
+  email: string;
+  private token: string;
+  private tokenExpirationDate: Date;
 
   constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
+    this.email = "";
+    this.token = "";
+    this.tokenExpirationDate = new Date();
   }
+
+  /*constructor(id: number, name: string, email: email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.token = "";
+  }*/
 }
