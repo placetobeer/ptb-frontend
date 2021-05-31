d9338394b3fb6610eb85118feb52aa4c5318629d
lucie <weber_lucie@web.de>
2021-05-31 16:25:40 +0200

    PTB-102: make memberlist dynamic to fill with invitations or memberships

--- src/app/hubpage/group-navigation-bar/member-list/member-list.component.ts
+++ src/app/hubpage/group-navigation-bar/member-list/member-list.component.ts
@@ -4,6 +4,9 @@ import {GroupRole} from '../../../entities/groupRole.enum';
 import {User} from '../../../entities/user.model';
 import {InvitationService} from "../../../services/invitation.service";
 import {AccountService} from "../../../services/account.service";
+import {MembershipService} from "../../../services/membership.service";
+import {GroupService} from "../../../services/group.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-member-list',
@@ -13,10 +16,15 @@ import {AccountService} from "../../../services/account.service";
 export class MemberListComponent implements OnInit {
   role = GroupRole;
   owner: User = this.accountService.user;
+  showInvitations;
 
-  constructor(public invitationService: InvitationService, private accountService: AccountService) { }
+  constructor(public invitationService: InvitationService, public membershipService: MembershipService,
+              public groupService: GroupService, private accountService: AccountService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.data.subscribe(data => {
+      this.showInvitations = data.showInvitations;
+    });
   }
 
 }
