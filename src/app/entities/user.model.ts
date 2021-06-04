7f628b751fab6bfed77ff140bc7aba9a4caad2df
bloup-dv <naujox.tom@gmail.com>
2021-06-04 15:37:01 +0200

    PTB-153: added autoLogin function

--- src/app/entities/user.model.ts
+++ src/app/entities/user.model.ts
@@ -2,24 +2,21 @@ export class User {
   id: number;
   name: string;
   email: string;
-  private token: string;
+  token: string;
   private tokenExpirationDate: Date;
 
-  constructor(id: number, name: string) {
+  constructor(id: number, name: string, email: string, token: string, tokenExpirationDate: Date) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    this.token = token;
+    this.tokenExpirationDate = tokenExpirationDate;
+  }
+
+  /*constructor(id: number, name: string) {
     this.id = id;
     this.name = name;
     this.email = "";
     this.token = "";
-    this.tokenExpirationDate = new Date();
-  }
-
-  /*constructor(id: number, name: string, email: email) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.token = "";
   }*/
-  public getToken(): string{
-    return this.token;
-  }
 }
