eecce087b2912418018806f22630f174d47fd547
bloup-dv <naujox.tom@gmail.com>
2021-06-05 03:28:45 +0200

    PTB-150: reworked login and registration page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,9 +9,8 @@ import {LoginComponent} from "./auth/login/login.component";
 import {RegisterComponent} from "./auth/register/register.component";
 
 const routes: Routes = [
-  {path: 'login', component: LoginComponent, children: [
-      {path: 'register', component: RegisterComponent}
-    ]},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'hubpage', component: HubpageComponent, children: [
       {path: 'new', component: CreateGroupPopupComponent}
     ]},
