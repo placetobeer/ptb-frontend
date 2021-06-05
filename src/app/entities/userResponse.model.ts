c741de829642332cd285d3b6789263cdecb6437a
bloup-dv <naujox.tom@gmail.com>
2021-06-05 10:31:21 +0200

    PTB-153: added UserResponse

---
+++ src/app/entities/userResponse.model.ts
@@ -0,0 +1,13 @@
+import {User} from "./user.model";
+
+export class UserResponse {
+  id: number;
+  name: string;
+  email: string;
+
+  constructor(user: User) {
+    this.id = user.id;
+    this.name = user.name;
+    this.email = user.email;
+  }
+}
